faf4ac5dc99500a95b54eb35a3e81fa761b7b214
Geovanny Ortiz <geo.g.ortiz@gmail.com>
2024-11-12 21:48:05 -0500

    Added page not found

--- client/src/pages/NoPage.jsx
+++ client/src/pages/NoPage.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import logo from '../assets/images/logo.png'
+import React, {useLayoutEffect} from 'react'
+import { Link } from 'react-router-dom';
+import notfound from '../assets/images/404.png';
 import '../App.css'
 
-function NoPage() {
 
+function NoPage({setShowNavbar}) {
+  useLayoutEffect(() => {
+    setShowNavbar(false);
+  }, [])
+  
   return (
     <>
-      <div>
-        <a href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
-          <img src={logo} className="logo react" alt="Congregator logo" />
-        </a>
-      </div>
-      <p className="read-the-docs">
-        Your One Stop for all your UF Student Organization Needs!
+      <Link to = {"/"}>
+        <img src={notfound} alt="Page not found"/>
+      </Link>
+      <p style={{fontSize: "50px"}}>
+          404 Page not Found! 
       </p>
     </>
   )
